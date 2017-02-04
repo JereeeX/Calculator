865106d6f89a321bfeac83c47f551fbb8780d145
JereeeX <yee.leaves@gmail.com>
2017-02-04 17:57:44 -0500

    create
    first commit

---
+++ Calculator/CalculatorBrain.swift
@@ -0,0 +1,88 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Jeremy.Xu on 2016-11-05.
+//  Copyright © 2016 Yi Hsu. All rights reserved.
+//
+
+import Foundation
+
+
+func multiply(op1: Double, op2: Double) -> Double{
+    return op1*op2
+}
+class CalculatorBrain{
+    private var accumulator = 0.0
+    
+    func setOperand(operand: Double) {
+        accumulator = operand
+    }
+    
+    var operations: Dictionary<String, Operation> = [
+        "π": Operation.Constant(M_PI),
+        "e": Operation.Constant(M_E),
+        "√": Operation.UnaryOperation(sqrt),
+        "cos": Operation.UnaryOperation(cos),
+        "sin": Operation.UnaryOperation(sin),
+        "tan": Operation.UnaryOperation(tan),
+        "x²": Operation.UnaryOperation( { $0*$0 } ),
+        "x³": Operation.UnaryOperation( { $0*$0*$0 } ),
+        "+": Operation.BinaryOperation( { $0 + $1 } ),
+        "-": Operation.BinaryOperation( { $0 - $1 } ),
+        "×": Operation.BinaryOperation( { $0 * $1 } ),
+        "÷": Operation.BinaryOperation( { $0 / $1 } ),
+        "=": Operation.Equals,
+        "c": Operation.Cancel
+    ]
+    
+    enum Operation{
+        case Constant(Double)
+        case UnaryOperation((Double) -> Double)
+        case BinaryOperation((Double, Double) -> Double)
+        case Equals
+        case Cancel
+    }
+    
+    func performOperation(symbol: String) {
+        if let operation = operations[symbol]{
+            switch operation {
+            case .Constant(let constValue):
+                accumulator = constValue
+            case .UnaryOperation(let function):
+                accumulator = function(accumulator)
+            case .BinaryOperation(let function):
+                executePendingOperation()
+                pending = PendingBinaryOperationInfo(binaryFunction: function, firstOperand: accumulator)
+            case .Equals:
+                executePendingOperation()
+            case .Cancel:
+                accumulator = 0.0
+            }
+            
+        }
+        
+    }
+    
+    private func executePendingOperation() {
+        if pending != nil{
+            accumulator = pending!.binaryFunction(pending!.firstOperand, accumulator)
+            pending = nil
+        }
+    }
+    
+    private var pending: PendingBinaryOperationInfo?
+    
+    private struct PendingBinaryOperationInfo {
+        var binaryFunction: (Double, Double) -> Double
+        var firstOperand: Double
+    }
+    
+    
+    
+    var result: Double {
+        get{
+            return accumulator
+        }
+    }
+}
---
+++ Calculator/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Jeremy.Xu on 2016-11-05.
+//  Copyright © 2016 Yi Hsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak private var display: UILabel!
+    
+    private var userIsInTheMiddleOfTyping = false
+    
+    
+    @IBAction private func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        
+        if userIsInTheMiddleOfTyping && display.text! != "0"{
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        }
+        else {
+            display.text = digit
+        }
+        
+        userIsInTheMiddleOfTyping = true
+        
+    }
+    
+    private var displayValue: Double {
+        get{
+            return Double(display.text!)!
+        }
+        set{
+            display.text = String(newValue)
+        }
+    }
+    
+    private var brain = CalculatorBrain()
+    
+    @IBAction private func performOperation(_ sender: UIButton) {
+        if userIsInTheMiddleOfTyping{
+            brain.setOperand(operand: displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        if let mathmaticalSymbol = sender.currentTitle{
+            brain.performOperation(symbol: mathmaticalSymbol)
+        }
+        displayValue = brain.result
+    }
+    
+}
